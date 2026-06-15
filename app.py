b98498d48cddbff4c08339799f8afd88e50fe1fb
Matteo Marchesini <matteo.marchesini12@gmail.com>
2026-06-15 16:07:12 +0000

    fix

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ st.markdown("""
 /* Contenitore del frame con altezza fissa e blocco verticale */
     .iframe-container {
         width: 100%;
-        height: 780px;      /* Altezza controllata del blocco */
+        height: 950px;      /* Altezza controllata del blocco */
         overflow-x: auto;   /* Attiva SOLO lo scroll orizzontale */
         overflow-y: hidden; /* Taglia e blocca lo scroll verticale */
         border-radius: 10px;
@@ -93,9 +93,8 @@ with tab3:
     st.subheader("Tabellone ad Eliminazione")
     embed_tabellone = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/htmlembed?gid={GID_TABELLONE}&range=A1:S32&widget=false&chrome=false&headers=false&rm=minimal"
     
-    # Il container CSS ora domina l'altezza e blocca l'asse Y
     st.markdown('<div class="iframe-container">', unsafe_allow_html=True)
-    st.components.v1.iframe(embed_tabellone, height=750, scrolling=False)
+    st.components.v1.iframe(embed_tabellone, height=900, scrolling=False) # <--- Portato a 900
     st.markdown('</div>', unsafe_allow_html=True)
 
 # --- TAB 4: RICERCA SQUADRA ---
